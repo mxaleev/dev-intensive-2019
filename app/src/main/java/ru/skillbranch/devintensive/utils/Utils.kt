e10e19317ba13e823f73b8ac677e28958f5fdbae
Mikhail Khaleev <web@strata.ru>
2019-07-03 14:07:35 +0300

    Start hometask_2

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,14 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?, String?> {
+        //TODO FIX ME!!!
+        var parts : List<String>? = fullName?.split(" ")
+
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+
+//        return Pair(firstName, lastName)
+        return firstName to lastName
+    }
+}
